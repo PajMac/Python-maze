d4ab2c35a8d6c4d7db65c84de0be6963fba550de
Pavel Galik <paja.galik.pg@gmail.com>
2020-03-29 17:49:06 +0200

    basic čáry

--- maze.py
+++ maze.py
@@ -1,1 +1,22 @@
+import turtle
 
+t = turtle.Turtle()
+t.speed(1)
+for i in range(3):
+    t.forward(100)
+    t.penup()
+    t.left(90)
+    t.forward(50)
+    t.left(90)
+    t.pendown()
+    t.forward(100)
+    t.penup()
+    t.right(90)
+    t.forward(50)
+    t.right(90)
+    t.pendown()
+    t.forward(1)
+
+
+
+turtle.done()
