0a4e8f0dbf40bdb906f85660093c297a1f6389ce
David Jezek <dawydj3@seznam.cz>
2020-04-29 13:35:56 +0200

    přidal jsem jeden řádek

--- funkce.py
+++ funkce.py
@@ -1,9 +1,59 @@
 import pygame
+import random
+import time
+
+slozitost = input("Zadejte složitost bludiště (od 5 do 40):")
+
+slozitost = int(slozitost)
+while slozitost < 5 or slozitost > 40:
+    slozitost = input("Zadej znovu:")
+    slozitost = int(slozitost)
+
+# pygame nastavení obrazu
+sirka = slozitost * 20 + 80
+vyska = slozitost * 20 + 70
+
+white = (255, 255, 255)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+yellow = (255, 255, 0)
+black = (0, 0, 0)
+
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((sirka, vyska))
+clock = pygame.time.Clock()
+image = pygame.image.load("robot_PNG.png")
+pygame.display.set_caption("Robot maze")
+
+robot = pygame.image.load("robot_PNG.png")
+robot = pygame.transform.scale(robot, (20, 20))
+
+bg_color = (125, 125, 125)
+screen.fill(bg_color)
+
+font = pygame.font.Font("freesansbold.ttf", 18)
+text = font.render("End", True, (0, 0, 0), bg_color)
+screen.blit(text, (0, 0))
+text = font.render("Start", True, (0, 0, 0), bg_color)
+screen.blit(text, ((slozitost * 20) + 30, (slozitost * 20) + 20))
+
+
+# volba proměnných pro bludiště
+x = 0
+y = 0
+w = 20  # tloušťka buňky
+grid = []
+visited = []
+stack = []
+solution = {}
+robotx = x
+roboty = y
 
 def main():
-    slozitost = input("Zadejte složitost bludiště (Alespoň 20):")
     # vytvoření mřížky
     def grid_setup(x, y, w):
+
         for i in range(1, int(slozitost) + 1):  # generuje bludiště 20x20
             x = 20  # zadani x souradnic na zacatek
             y = y + x  # začátek nové řady
@@ -16,43 +66,58 @@ def main():
                 grid.append((x, y))
                 x = x + 20
 
-    # pohyby
+
+# pohyby
     def nahoru(x, y):
-        pygame.draw.rect(screen, white, (x + 1, y - w + 1, 19, 39), 0)  # draw a rectangle twice the width of the cell
-        pygame.display.update()  # to animate the wall being removed
+        pygame.draw.rect(screen, white, (x + 1, y - w + 1, 19, 39), 0)
+        pygame.display.update()
+        pygame.event.pump()
+
 
     def dolu(x, y):
         pygame.draw.rect(screen, white, (x + 1, y + 1, 19, 39), 0)
         pygame.display.update()
+        pygame.event.pump()
+
 
     def doleva(x, y):
         pygame.draw.rect(screen, white, (x - w + 1, y + 1, 39, 19), 0)
         pygame.display.update()
+        pygame.event.pump()
+
 
     def doprava(x, y):
         pygame.draw.rect(screen, white, (x + 1, y + 1, 39, 19), 0)
         pygame.display.update()
+        pygame.event.pump()
+
 
     def bunka_setup(x, y):
         pygame.draw.rect(screen, green, (x + 1, y + 1, 18, 18), 0)
         pygame.display.update()
+        pygame.event.pump()
+
 
     def backtracking(x, y):
         pygame.draw.rect(screen, white, (x + 1, y + 1, 18, 18), 0)
         time.sleep(0.05)
         pygame.display.update()
+        pygame.event.pump()
+
 
     def reseni(x, y):
-        # pygame.draw.rect(screen, yellow, (x + 8, y + 8, 5, 5), 0,) # ukáže řešení
         screen.blit(robot, (x, y))
         time.sleep(0.1)
         pygame.display.update()
+        pygame.event.pump()
+
 
     # vykreslení bludiště
     def vykresleni(x, y):
         bunka_setup(x, y)  # počátek bludiště
         stack.append((x, y))
         visited.append((x, y))
+
         while len(stack) > 0:
             time.sleep(.01)
             bunka = []
@@ -102,10 +167,13 @@ def main():
             reseni(x, y)
         time.sleep(.1)
 
+    x, y = 20, 20  # počáteční pozice mřížky
+    grid_setup(20, 20, 20)
+    vykresleni(x, y)
+    cesta_zpet((20 * slozitost), (20 * slozitost) + 20)
 
-    x, y = 20, 20  # starting position of grid
-    grid_setup(20, 20, 20)  # 1st argument = x value, 2nd argument = y value, 3rd argument = width of cell
-    vykresleni(x, y)  # call build the maze  function
-    cesta_zpet((20 * slozitost), (20 * slozitost) + 20)  # call the plot solution function
+
 if __name__ == "__main__":
     main()
+
+
