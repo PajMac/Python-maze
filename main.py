3a5a0590d9c34f42f480e2f52d1522b4da18e4d7
Pavel Galik <paja.galik.pg@gmail.com>
2020-05-01 13:53:38 +0200

    rename

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import pygame
 funkce.main()
 running = True
 while running:
+    clock = pygame.time.Clock()
+    clock.tick(120)
     for event in pygame.event.get():
         pygame.event.pump()
         if event.type == pygame.QUIT:
